2ebc5e42f6605602c166092eeb1f0527e01b61a5
djessycousto <79341027+djessycousto@users.noreply.github.com>
2025-02-16 13:08:53 +0000

    navbar css and js. index css bug fixed

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
-console.log("hello ");
+// navebar
+
+const menu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".close-menu");
+const linksContainer = document.querySelector(".links-container");
+const overlay = document.querySelector(".overlay");
+
+menu.addEventListener("click", () => {
+  overlay.classList.remove("show-menu");
+  overlay.classList.add("show-menu");
+});
+
+closeMenu.addEventListener("click", () => {
+  overlay.classList.remove("show-menu");
+});
